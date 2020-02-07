7f2bd474e0c556432bdb63d7d5f9169e5bb46424
john.oliveros <John.Oliveros@wolterskluwer.com>
2020-02-07 15:51:01 -0500

    sync course changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-// import react and reactdom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
-// create a react component
 const App = () => {
-    // return <div>Hi there!</div>;
-    const buttonText = { text: 'Click me!'};
-    const labelText = 'Enter name: ';
-
     return (
-        <div>
-            <label className="label" htmlFor="name">{labelText}</label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        <div className="ui container comments">            
+            <CommentDetail />
         </div>
-
     );
 }
 
-// take the react component, show in screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
